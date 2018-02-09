256d310e619a8a3eeaa8e1ccd675b78e61d5f514
Ershad Rahimikia <Ershad.Rahimikia@D2L.com>
2018-02-09 17:16:32 -0500

    adding ui changes

---
+++ shakespeare.ui/Options.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+
+namespace shakespeare.ui {
+
+	[Verb( "add", HelpText = "Add new todo items." )]
+	class AddOptions {
+
+		[Option( 'i', "input", Required = true, HelpText = "Todo item description." )]
+		public string Description { get; set; }
+	}
+
+	[Verb( "delete", HelpText = "Delete existing todo items." )]
+	class DeleteOptions {
+		//normal options here
+	}
+
+	[Verb( "list", HelpText = "List all todo items." )]
+	class ListOptions {
+		//normal options here
+	}
+
+	[Verb( "exit", HelpText = "Exit Shakespeare." )]
+	class ExitOptions {
+		//normal options here
+	}
+
+}
--- shakespeare.ui/Program.cs
+++ shakespeare.ui/Program.cs
@@ -1,12 +1,79 @@
 ﻿using System;
+using CommandLine;
+using shakespeare.core;
 
-namespace shakespeare.ui
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+namespace shakespeare.ui {
+	class Program {
+
+		private static bool m_exit = false;
+		private static ITodoManager m_todoManager = new TodoManager();
+
+		static void Main( string[] args ) {
+
+			PrintHeader();
+
+			while( !m_exit ) {
+
+				Console.Write("> ");
+				string inputStr = Console.ReadLine();
+
+				if( string.IsNullOrEmpty( inputStr ) ) {
+					continue;
+				}
+
+				string[] input = inputStr.Split( ' ' );
+
+				Console.Clear();
+
+				PrintHeader();
+
+				Parser.Default.ParseArguments<AddOptions, DeleteOptions, ListOptions>( input )
+					.WithParsed<AddOptions>( opts => RunAddAndReturnExitCode( opts ) )
+					.WithParsed<DeleteOptions>( opts => RunDeleteAndReturnExitCode( opts ) )
+					.WithParsed<ListOptions>( opts => RunListAndReturnExitCode( opts ) )
+					.WithParsed<ExitOptions>( opts => RunExitAndReturnExitCode( opts ) )
+					.WithNotParsed( errs => {
+						Console.WriteLine( "Invalid command!" );
+					} );
+
+			}			
+		}
+
+		private static void PrintHeader() {
+			Console.WriteLine( "Welcome to Shakespeare!" );
+			Console.WriteLine( "Todo or not Todo. That is the question." );
+			Console.WriteLine( "To see a list of all available commands type help." );
+			Console.WriteLine( "-----------------------------------------------------\n" );
+		
+		}
+
+		private static object RunExitAndReturnExitCode( ExitOptions opts ) {
+			m_exit = true;
+			Console.WriteLine( "Farewell ......" );
+			return 0;
+		}
+
+		private static object RunAddAndReturnExitCode( AddOptions opts ) {
+
+			m_todoManager.SaveItem( opts.Description );
+
+			Console.WriteLine("Item sucessfully added.");
+			return 0;
+		}
+
+		private static object RunDeleteAndReturnExitCode( DeleteOptions opts ) {
+			Console.WriteLine( "Item deleted." );
+			return 0;
+		}
+
+		private static object RunListAndReturnExitCode( ListOptions opts ) {
+			Console.WriteLine( "List of Todo items:\n" );
+
+			foreach( var item in m_todoManager.GetItems() ) {
+				Console.WriteLine(item.Description);
+			}
+			return 0;
+		}
+
+	}
 }
